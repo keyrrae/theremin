6d5dec40167057e1f5abc44ab6284c71cd25b1f4
Xuan Wang <xuan-wang@outlook.com>
2015-12-02 22:17:18 -0800

    Update widthDet.v

--- widthDet.v
+++ widthDet.v
@@ -1,9 +1,13 @@
 module widthDet(vin, reset, clk, out);
 
 input vin;
+input clk;
+input reset;
 output [12:0] out;
+reg [12:0] out;
 
-reg [12:0] width
+reg [12:0] width;
+reg [6:0] counter;
 
 reg state;
 reg next_state;
@@ -15,38 +19,43 @@ parameter COUNT = 1;
 always @(posedge clk) begin
     if(reset) begin
         state <= IDLE;
-        counter <= 0;
+        counter <= 1;
         width <=0;
         out <= 300;
+	next_state <= IDLE;
     end
 
-    else if(counter == 100) begin
+    else if(counter == 99) begin
         counter <= 0;
 
         case(state)
             IDLE: begin
                 if (vin == 1'b1) begin
-                    next_state <= count;
+                    state <= COUNT;
                     width = 1;
                 end
+	    end
             COUNT: begin
                 if(vin == 1'b1)
                     width <= width + 1;
                 else begin
-                    next_state <= IDLE;
+                    state <= IDLE;
                     out <= width;
                 end
-
+	    end
             default: begin
-                next_state <= IDLE;
-                counter <= 0;
+                state <= IDLE;
+                counter <= 1;
                 width <= 0;
                 out <= 300;
             end
         endcase
-        state <= next_state;
+        
     end
-
+    else begin
+	counter <= counter + 1;
+end
+	
 
 end
 
